a3b25756fbd4776eef9a772e240bc252782c2d17
Manuel028 <manuelvictorino70@gmail.com>
2026-02-20 21:05:39 +0200

    atualizei

--- exercicios/biolife02/script.js
+++ exercicios/biolife02/script.js
@@ -5,6 +5,24 @@ const imagens = [
   "biolife_pictures1/biolife6ave.jpeg"
 ];
 
+function clickMenu(){
+            if (itens.style.display == 'block'){
+                itens.style.display = 'none'
+            
+            } else {
+                itens.style.display = 'block'
+            }
+
+        }
+    
+    function mudouTamanho() {
+            if (window.innerWidth >= 600) {
+                itens.style.display = 'block'
+            } else {
+                itens.style.display = 'none'
+            }
+        }    
+
 let index = 0;
 const hero = document.querySelector(".hero");
 
